ef852f64ede62b6d28d8705fa927a4a9de206f8f
Ardy Dwi Krisnanto <ardy.dwik@gmail.com>
2020-07-31 16:40:58 +0700

    animal done

--- animal.php
+++ animal.php
@@ -1,31 +1,28 @@
 <?php 
 
+require "ape.php";
+require "frog.php";
 
 class animal{
 	public $name;
-	public $legs;
-	public $cold_blooded;
+	public $legs=2;
+	public $cold_blooded='false';
 
-	public function set_name($name){
-		$this->name=$name;
+	public function __construct($name){
+		$this->name = $name;
 	}
 	public function get_name(){
-		return $this->name;
-	}
-
-	public function set_legs(){
-		$this->legs=2;
+		return 'Nama binatang ini adalah '.$this->name;
 	}
 	public function get_legs(){
-		return $this->legs;
-	}
-
-	public function set_cold_blooded(){
-		$this->cold_blooded=false;
+		return "<br>Jumlah Kakinya ".$this->legs;
 	}
 	public function get_cold_blooded(){
-		return $this->false;
+		return "<br>hewan ini berdarah dingin? ".$this->cold_blooded;
 	}
+}
+class ape extends animal {
+	
 }
 
  ?>
--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 
-require_once "animal.php";
+require "animal.php";
 
 $sheep = new animal("shaun");
 
-echo 'bunyio'.$sheep->get_name(); // "shaun"
-echo $sheep->legs; // 2
-echo $sheep->cold_blooded; // false
-
-
+echo $sheep->get_name(); // "shaun"
+echo $sheep->get_legs(); // 2
+echo $sheep->get_cold_blooded(); // false
+echo "<br>--";
 
 // NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
 
+$sungokong = new ape("kera sakti");
+$sungokong->get_name() // "Auooo"
+/*
+$kodok = new frog("buduk");
+$kodok->jump() ; // "hop hop"*/
 
 
 
